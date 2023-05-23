e3d246f5c350f356822ee25f6df9493772fd8043
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-23 20:32:00 +0300

    Updates

---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export type FetchResponse<data, param> = {
+    response: data | null;
+    error: any;
+    loading: boolean;
+    fetcher: (arg: param, token?: string, isFormData?: boolean) => any;
+};
